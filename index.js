70509549bc7058918d8099dded9bb94f8d1cfa15
Steve Murigi <steve.murigi@student.moringaschool.com>
2024-07-11 21:19:30 -0500

    Looping lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    const cards = [];
+    
+    for (let i = 0; i < names.length ; i++){
+        cards.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards;
+}
+  
+
+function countDown(number){
+    let i = 10;
+    while (i >= 0){
+        console.log (i);
+        i--
+    }
+    return number;
+}
